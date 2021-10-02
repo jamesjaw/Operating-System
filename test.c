b6975275681fdae054f65b9cf40b62cf51389caf
jamesjaw <50652557+jamesjaw@users.noreply.github.com>
2021-10-03 01:38:34 +0800

    Update test.c
    final

--- test.c
+++ test.c
@@ -21,7 +21,16 @@ int main(void)
         */
         char com[MAX_LINE];
         pid_t pid;
-        read(0, com, MAX_LINE/2+1);
+	/*
+        fgets(com,MAX_LINE,stdin);
+	com[strlen(com)-1] = '\0';
+	*/
+	
+	read(fileno(stdin),com,MAX_LINE);
+	int s = 0;
+	while(com[s] != '\n') s++;
+	if(com[s] == '\n') com[s] = '\0';
+	
         arg[0] = strtok(com," ");
         
         int count = 0;
@@ -30,7 +39,7 @@ int main(void)
         }
         
         
-        if(strcmp(com,"exit\n") == 0){
+        if(strcmp(com,"exit\0") == 0){
             return 0;
         }
         else{
@@ -40,13 +49,13 @@ int main(void)
                 char* temp = NULL;
                 temp = strstr(arg[0], "\n");
                 *temp = '\0';
-                */
+                
                 
                 char z[MAX_LINE];
                 strcpy(z, arg[0]);
                 z[strlen(z)-1] = '\0';
-                
-                execvp(z, arg);
+                */
+                execvp(arg[0], arg);
             }
             else if(pid > 0){
                 wait(NULL);
