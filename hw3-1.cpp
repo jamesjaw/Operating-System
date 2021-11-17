d47fc7787e2903f39d08c00c6fc1c67ff1bb8d8a
jamesjaw <50652557+jamesjaw@users.noreply.github.com>
2021-11-17 21:10:40 +0800

    Update hw3-1.cpp

--- hw3-1.cpp
+++ hw3-1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <pthread.h>
 
 using namespace std;
@@ -25,8 +26,44 @@ void* bubble(int input){
     return NULL;
 }
 
-void* merge(void* input){
+void* merge(int input, int por){
+    int amount = countz / por;
+    int start_L = amount * input;
+    int start_R = amount * (input+1);
+    int end_L = start_L + amount;
+    int end_R = start_R + amount;
+    if(input == por - 1) end_R += (countz % por);
     
+    int start = start_L;
+    vector<int> temp;
+    while(1){
+        if(start_L == end_L){
+            while(start_R != end_R){
+                temp.push_back(data[start_R]);
+                start_R++;
+            }
+            break;
+        }
+        if(start_R == end_R){
+            while(start_L != end_L){
+                temp.push_back(data[start_L]);
+                start_L++;
+            }
+            break;
+        }
+        if(data[start_L] < data[start_R]){
+            temp.push_back(data[start_L]);
+            start_L++;
+        }
+        else{
+            temp.push_back(data[start_R]);
+            start_R++;
+        }
+    }
+    for(int i:temp){
+        data[start] = i;
+        start++;
+    }
     return NULL;
 }
 
@@ -38,6 +75,7 @@ int main(){
     while(cin>>data[countz]){
         countz++;
     }
+    
     num = countz / 4;
     lest = countz % 4;
     bubble(0);
@@ -45,7 +83,10 @@ int main(){
     bubble(2);
     bubble(3);
     
-    
+    merge(0, 4);
+    merge(2, 4);
+    merge(0, 2);
+ 
     for(int i=0;i<countz;i++) cout<<data[i]<<" ";
     return 0;
 }
