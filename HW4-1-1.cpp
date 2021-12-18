770f55219f3d2fa9e70b0592257dda14d6771684
jamesjaw <50652557+jamesjaw@users.noreply.github.com>
2021-12-19 00:41:04 +0800

    Update HW4-1-1.cpp

--- HW4-1-1.cpp
+++ HW4-1-1.cpp
@@ -4,6 +4,10 @@
 #include <string>
 using namespace std;
 
+pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m2 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t m3 = PTHREAD_MUTEX_INITIALIZER;
+
 string Q;
 int p_num;
 int lenth;
@@ -13,17 +17,23 @@ void* clu(void* input){
     int amount = lenth / p_num;
     
     long start = (long)input * amount;
-    if((long)input == p_num - 1) amount += lenth % p_num;
+    //if((long)input == p_num - 1) amount += lenth % p_num;
     
     for(long i=start;i<amount;i++){
         if(Q[i] == '0'){
+            pthread_mutex_lock(&m1);
             countz[0]++;
+            pthread_mutex_unlock(&m1);
         }
         else if(Q[i] == '1'){
+            pthread_mutex_lock(&m2);
             countz[1]++;
+            pthread_mutex_unlock(&m2);
         }
         else if(Q[i] == '2'){
+            pthread_mutex_lock(&m3);
             countz[2]++;
+            pthread_mutex_unlock(&m3);
         }
     }
     pthread_exit(0);
@@ -46,7 +56,7 @@ int main(){
     }
     
     for(int i=0;i<3;i++){
-        cout<<i<<" "<<countz[i]<<"\n";
+        cout<<i<<": "<<countz[i]<<"\n";
     }
     
     return 0;
